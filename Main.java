8aa4d58829038095b8ee0eb312e06b0d3dba51b5
bhagirathlodhi <115892708+bhagirathlodhi@users.noreply.github.com>
2022-11-04 01:05:00 +0530

    main class

---
+++ Main.java
@@ -0,0 +1,16 @@
+package emp.org;
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner myObj = new Scanner(System.in);
+    String userName;
+    
+    // Enter username and press Enter
+    System.out.println("Enter username"); 
+    userName = myObj.nextLine();   
+       
+    System.out.println("Username is: " + userName);        
+  }
+}
